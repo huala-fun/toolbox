5eb9532b076148e1a416a1b1b453ae1e35d35ef1
= <huala.fun@outlook.com>
2024-02-25 23:04:58 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <el-button
-      link
-      >111</el-button
-    >
+    <img alt="Vue logo" class="logo" src="/favicon.ico" width="125" height="125" />
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld msg="开发工具箱" />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
